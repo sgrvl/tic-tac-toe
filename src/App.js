f08ad20fe1c9347b1fe3e667112b1aaedb1e63c9
Simon Gravel <simgrvl@gmail.com>
2020-10-20 15:18:50 -0400

    removed every useState

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useReducer, useEffect } from "react";
+import React, { useReducer, useEffect } from "react";
 import { createGlobalStyle } from "styled-components";
 import Board from "./Board";
 import Square from "./Square";
@@ -16,6 +16,7 @@ export const ACTIONS = {
 	X: "X",
 	O: "O",
 	RESET: "RESET",
+	OVER: "OVER",
 };
 
 function reducer(state, action) {
@@ -24,17 +25,31 @@ function reducer(state, action) {
 			return {
 				player: "O",
 				squares: state.squares,
+				isOver: false,
+				winner: `Player: O`,
 			};
 		case ACTIONS.O:
 			return {
 				player: "X",
 				squares: state.squares,
+				isOver: false,
+				winner: `Player: X`,
 			};
 		case ACTIONS.RESET:
 			return {
 				player: "X",
 				squares: ["", "", "", "", "", "", "", "", ""],
+				isOver: false,
+				winner: `Player: X`,
 			};
+		case ACTIONS.OVER:
+			return {
+				player: state.player,
+				squares: state.squares,
+				isOver: true,
+				winner: action.payload,
+			};
+
 		default:
 			console.log("error");
 	}
@@ -52,46 +67,35 @@ const WINNING_CONDITIONS = [
 ];
 
 function App() {
-	const [isOver, setIsOver] = useState(false);
-	const [winner, setWinner] = useState("It's a tie!");
 	const [state, dispatch] = useReducer(reducer, {
 		player: "X",
 		squares: ["", "", "", "", "", "", "", "", ""],
+		isOver: false,
+		winner: `Player: X`,
 	});
 
 	useEffect(() => {
-		const space = state.squares.filter((s) => s === "");
-		if (space.length === 0) {
-			setIsOver(true);
-		}
 		const test_X = WINNING_CONDITIONS.some((combo) =>
 			combo.every((index) => state.squares[index] === "X")
 		);
 		const test_O = WINNING_CONDITIONS.some((combo) =>
 			combo.every((index) => state.squares[index] === "O")
 		);
-		if (test_X) {
-			setWinner("Winner is X!");
-			setIsOver(true);
-		}
-		if (test_O) {
-			setWinner("Winner is O!");
-			setIsOver(true);
+		if (!state.isOver) {
+			if (!state.squares.includes(""))
+				dispatch({ type: ACTIONS.OVER, payload: "It's a tie!" });
+			if (test_X) dispatch({ type: ACTIONS.OVER, payload: "X wins!" });
+			if (test_O) dispatch({ type: ACTIONS.OVER, payload: "O wins!" });
 		}
 	}, [state]);
 
 	return (
 		<Context.Provider value={{ state, dispatch }}>
 			<h1>Tic Tac Toe</h1>
-			{isOver ? <p>{winner}</p> : <p>Player: {state.player}</p>}
+			<p>{state.winner}</p>
 			<Board>
 				{state.squares.map((value, index) => (
-					<Square
-						value={value}
-						index={index}
-						key={`Square: ${index}`}
-						isOver={isOver}
-					/>
+					<Square value={value} index={index} key={`Square: ${index}`} />
 				))}
 			</Board>
 			<Reset onClick={() => dispatch({ type: ACTIONS.RESET })}>Reset</Reset>
--- src/Square.js
+++ src/Square.js
@@ -6,11 +6,11 @@ const StyledSquare = styled.button`
 	font-size: 5rem;
 `;
 
-const Square = ({ value, index, isOver }) => {
+const Square = ({ value, index }) => {
 	const { state, dispatch } = useContext(Context);
 
 	function handleClick() {
-		if (!isOver && state.squares[index] === "") {
+		if (!state.isOver && state.squares[index] === "") {
 			state.squares[index] = state.player;
 			if (state.player === "X") {
 				dispatch({ type: ACTIONS.X });
