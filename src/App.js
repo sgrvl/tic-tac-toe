6460aed3b7b7f8626c9a11e7b0b5ff8a34c18d5c
Simon Gravel <simgrvl@gmail.com>
2020-10-20 21:10:43 -0400

    line animation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { useReducer, useEffect, useState } from "react";
-import { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle, keyframes } from "styled-components";
 import Board from "./Board";
 import Square from "./Square";
-import styled from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -130,24 +129,39 @@ const Reset = styled.button`
 	margin: 2rem;
 `;
 
+const animationHorz = keyframes`
+	0% { width: 0%;}
+	100% { width: 90%;}
+`;
+
+const animationVer = keyframes`
+	0% { height: 0%;}
+	100% { height: 90%;}
+`;
+
+const animationDia = keyframes`
+	0% { width: 0%;}
+	100% { width: 120%;}
+`;
+
 const handleWinningLine = (combo) => {
 	switch (combo) {
 		case WINNING_CONDITIONS[0]:
-			return "top: calc(16.5% - 5px); height: 5px; width: 90%; margin-left: 5%;";
+			return `top: calc(16.5% - 5px); height: 5px; width: 90%; margin-left: 5%;`;
 		case WINNING_CONDITIONS[1]:
-			return "top: calc(50% - 5px); height: 5px; width: 90%; margin-left: 5%;";
+			return `top: calc(50% - 5px); height: 5px; width: 90%; margin-left: 5%;`;
 		case WINNING_CONDITIONS[2]:
-			return "bottom: calc(16.5% + 2px); height: 5px; width: 90%; margin-left: 5%;";
+			return `bottom: calc(16.5% + 2px); height: 5px; width: 90%; margin-left: 5%;`;
 		case WINNING_CONDITIONS[3]:
-			return "left: calc(16.5% - 2px); height: 90%; width: 5px; margin-top: 5%;";
+			return `left: calc(16.5% - 2px); height: 90%; width: 5px; margin-top: 5%;`;
 		case WINNING_CONDITIONS[4]:
-			return "left: calc(50% - 2.5px); height: 90%; width: 5px; margin-top: 5%;";
+			return `left: calc(50% - 2.5px); height: 90%; width: 5px; margin-top: 5%;`;
 		case WINNING_CONDITIONS[5]:
-			return "right: calc(16.5% - 1.5px); height: 90%; width: 5px; margin-top: 5%;";
+			return `right: calc(16.5% - 1.5px); height: 90%; width: 5px; margin-top: 5%;`;
 		case WINNING_CONDITIONS[6]:
-			return "height: 5px; width: 120%; margin: 7.5%; transform: rotate(45deg); transform-origin: 2px;";
+			return `height: 5px; width: 120%; margin: 7.5%; transform: rotate(45deg); transform-origin: 2px;`;
 		case WINNING_CONDITIONS[7]:
-			return "height: 5px; width: 120%; margin: 7.5%; transform: rotate(-45deg); transform-origin: 2px; bottom: 0;";
+			return `height: 5px; width: 120%; margin: 7.5%; transform: rotate(135deg); transform-origin: 2px; left: 84.5%;`;
 		default:
 			return "display: none";
 	}
@@ -157,6 +171,17 @@ const WinLine = styled.div`
 	box-sizing: border-box;
 	position: absolute;
 	background: black;
+	animation: 0.7s
+		${({ combo }) =>
+			combo === WINNING_CONDITIONS[0] ||
+			combo === WINNING_CONDITIONS[1] ||
+			combo === WINNING_CONDITIONS[2]
+				? animationHorz
+				: combo === WINNING_CONDITIONS[3] ||
+				  combo === WINNING_CONDITIONS[4] ||
+				  combo === WINNING_CONDITIONS[5]
+				? animationVer
+				: animationDia};
 	${({ combo }) => handleWinningLine(combo)}
 `;
 
